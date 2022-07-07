01b40118a7ce308ccf004b3bda342acf49fb84ea
Abdul Mannan <abdulmannan@unknown.home>
2022-07-07 13:17:06 +0100

    hook consumer to the app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	appkafka "github.com/mannanmcc/kafka-consumer/internal/kafka"
 )
 
 func main() {
 	fmt.Println("starting kafka consumer.....")
+
+	//run the consumer in the separate go routing to make sure it does not stop on this line as we need to execute rest of the code
+	go appkafka.StartConsumer()
+
+	fmt.Println("consumer started")
+
+	//keep it running for 10  mins
+	time.Sleep(10 * time.Minute)
 }
